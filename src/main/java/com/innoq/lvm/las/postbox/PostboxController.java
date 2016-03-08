56dd3096e459f067b3a2230a0b49283a893d1e48
Marc Jansing <marc.jansing@innoq.com>
2016-03-08 10:23:05 +0100

    add thymeleaf template engine

--- src/main/java/com/innoq/lvm/las/postbox/PostboxController.java
+++ src/main/java/com/innoq/lvm/las/postbox/PostboxController.java
@@ -2,7 +2,6 @@ package com.innoq.lvm.las.postbox;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by mjansing on 08/03/16.
@@ -10,8 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PostboxController {
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
-        return "Test";
+        return "index";
     }
 }
