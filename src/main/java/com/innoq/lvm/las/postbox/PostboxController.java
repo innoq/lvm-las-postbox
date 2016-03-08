c485e93d23f3cbcd63e4fa80d2229451de5c568f
Marc Jansing <marc.jansing@innoq.com>
2016-03-08 10:32:46 +0100

    add sample view content

--- src/main/java/com/innoq/lvm/las/postbox/PostboxController.java
+++ src/main/java/com/innoq/lvm/las/postbox/PostboxController.java
@@ -2,6 +2,9 @@ package com.innoq.lvm.las.postbox;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
 
 /**
  * Created by mjansing on 08/03/16.
@@ -9,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PostboxController {
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        return new ModelAndView("index", "messages", Arrays.asList("Foo", "Bar"));
     }
 }
