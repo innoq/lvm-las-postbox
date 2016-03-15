aa817861b3b22f1f82c77d4ddaf13452435d5fc8
Marc Jansing <marc.jansing@innoq.com>
2016-03-15 15:35:00 +0100

    add message show action

--- src/main/java/com/innoq/lvm/las/postbox/controller/MessagesController.java
+++ src/main/java/com/innoq/lvm/las/postbox/controller/MessagesController.java
@@ -3,6 +3,7 @@ package com.innoq.lvm.las.postbox.controller;
 import com.innoq.lvm.las.postbox.model.Message;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
@@ -22,8 +23,20 @@ public class MessagesController {
 		return new ModelAndView("messages/index", "messages", messages);
 	}
 
+	@RequestMapping(path = "messages/{messageId}", method = RequestMethod.GET)
+	public ModelAndView show(@PathVariable("messageId") long messageId) {
+		Message message = getMessageFromServiceByMessageId(messageId);
+		return new ModelAndView("messages/show", "message", message);
+	}
+
 	private Message[] getMessagesFromService() {
 		RestTemplate restTemplate = new RestTemplate();
 		return restTemplate.getForObject("http://localhost:5100/briefkasten/m50000/", Message[].class);
+	}
+
+	private Message getMessageFromServiceByMessageId(long messageId) {
+		RestTemplate restTemplate = new RestTemplate();
+		String serviceUri = "http://localhost:5100/briefkasten/m50000/" + messageId;
+		return restTemplate.getForObject(serviceUri, Message.class);
 	}
 }
