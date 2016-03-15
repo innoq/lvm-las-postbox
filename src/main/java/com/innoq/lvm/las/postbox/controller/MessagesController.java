9ae40d8590140724a9285902c3eb74c64938d7cc
Marc Jansing <marc.jansing@innoq.com>
2016-03-15 14:04:43 +0100

    add service based message overview

--- src/main/java/com/innoq/lvm/las/postbox/controller/MessagesController.java
+++ src/main/java/com/innoq/lvm/las/postbox/controller/MessagesController.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
 
 /**
  * Created by mjansing on 08/03/16.
@@ -19,9 +18,12 @@ public class MessagesController {
 
 	@RequestMapping(path = "/messages", method = RequestMethod.GET)
 	public ModelAndView index() {
-		ArrayList<Message> messages = new ArrayList();
-		messages.add(new Message("16.03.2015", "Wiedervorlage", "Vers 987654321", "http://localhost:8080/vertrag/987654321"));
-		messages.add(new Message("20.03.2015", "UB-Vorlage", "", ""));
+		Message[] messages = getMessagesFromService();
 		return new ModelAndView("messages/index", "messages", messages);
+	}
+
+	private Message[] getMessagesFromService() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.getForObject("http://localhost:5100/briefkasten/m50000/", Message[].class);
 	}
 }
--- src/main/java/com/innoq/lvm/las/postbox/model/Message.java
+++ src/main/java/com/innoq/lvm/las/postbox/model/Message.java
@@ -1,34 +1,101 @@
 package com.innoq.lvm.las.postbox.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created by mjansing on 09/03/16.
  */
-public class Message {
-	private final String date;
-	private final String text;
-	private final String referenceText;
-	private final String referenceUri;
 
-	public Message(String date, String text, String referenceText, String referenceUri) {
-		this.date = date;
-		this.text = text;
-		this.referenceText = referenceText;
-		this.referenceUri = referenceUri;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Message {
+	@JsonProperty("entryId") // explicit json mapping
+	private int entryId;
+
+	@JsonProperty("entryUri") // explicit json mapping
+	private String entryUri;
+
+	@JsonProperty("datum")
+	private String date;
+
+	@JsonProperty("text") // explicit json mapping
+	private String text;
+
+	@JsonProperty("bezug")
+	private String reference;
+
+	@JsonProperty("bezugId")
+	private String referenceId;
+
+	@JsonProperty("bezugsUri")
+	private String referenceUri;
+
+	public int getEntryId() {
+		return entryId;
+	}
+
+	public void setEntryId(int entryId) {
+		this.entryId = entryId;
+	}
+
+	public String getEntryUri() {
+		return entryUri;
+	}
+
+	public void setEntryUri(String entryUri) {
+		this.entryUri = entryUri;
 	}
 
 	public String getDate() {
 		return date;
 	}
 
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 	public String getText() {
 		return text;
 	}
 
-	public String getReferenceText() {
-		return referenceText;
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getReferenceId() {
+		return referenceId;
+	}
+
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
 	}
 
 	public String getReferenceUri() {
 		return referenceUri;
+	}
+
+	public void setReferenceUri(String referenceUri) {
+		this.referenceUri = referenceUri;
+	}
+
+	@Override
+	public String toString() {
+		return "Message{" +
+			"entryId=" + entryId +
+			", entryUri='" + entryUri + '\'' +
+			", date='" + date + '\'' +
+			", text='" + text + '\'' +
+			", reference='" + reference + '\'' +
+			", referenceId='" + referenceId + '\'' +
+			", referenceUri='" + referenceUri + '\'' +
+			'}';
 	}
 }
